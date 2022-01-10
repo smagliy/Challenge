8e34b9ebce426ad8668961cdbc5a9dc00ca28aad
Kateryna Smahlii <90623566+smagliy@users.noreply.github.com>
2022-01-10 17:35:14 +0200

    Task done

--- test.py
+++ test.py
@@ -1,69 +1,96 @@
-from RPA.Browser.Selenium import Selenium
-from RPA.Browser.Selenium import ChromeOptions
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import pandas as pd
 import os
 import time
 import xlsxwriter
 
-browser_lib = Selenium()
-options = ChromeOptions()
 path = os.path.dirname(os.path.abspath(__file__))
 prefs = {"download.default_directory": path}
+options = Options()
 options.add_experimental_option("prefs", prefs)
+driver = webdriver.Chrome(options=options)
+wait = WebDriverWait(driver, 10)
+
+
+def open_browser_and_click_btn():
+    driver.get('https://itdashboard.gov')
+    btn = driver.find_element_by_css_selector('a.btn.btn-default.btn-lg-2x')
+    btn.click()
+
+
+def search_for_information():
+    wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'a.btn.btn-default.btn-sm')))
+    dict_el = {
+        'button': [el.get_attribute('href') for el in driver.find_elements_by_css_selector(
+            'a.btn.btn-default.btn-sm')],
+        'name': [elem.text for elem in driver.find_elements_by_css_selector('span.h4.w200')
+                 if elem.text != ''],
+        'salary': [elem.text for elem in driver.find_elements_by_css_selector('span.h1.w900')]
+    }
+    return dict_el
+
+
+def search_agent(dict_info, name_agency):
+    index = dict_info['name'].index(name_agency)
+    return dict_info['button'][index]
+
+
+def agent_full_information(url):
+    driver.get(url)
+    wait.until(EC.element_to_be_clickable((
+        By.CSS_SELECTOR, 'select.form-control.c-select[aria-controls="investments-table-object"]')))
+    btn_select = driver.find_element_by_css_selector(
+        'select.form-control.c-select[aria-controls="investments-table-object"]')
+    btn_select.click()
+    btn_all = driver.find_element_by_css_selector(
+        'select.form-control.c-select[aria-controls] option[value="-1"]')
+    btn_all.click()
+    driver.implicitly_wait(10)
+    dict_full = {
+        'uii': [el.text for el in driver.find_elements_by_css_selector('td.left.sorting_2')],
+        'uii_href': [el.get_attribute('href') for el in
+                     driver.find_elements_by_css_selector('td.left.sorting_2 a')],
+        'total': [el_1.text for el_1 in driver.find_elements_by_css_selector('td.right')]
+    }
+    return dict_full
+
+
+def add_excel_agent(dict_full, dict_el):
+    df1 = pd.DataFrame(dict_el['name'],
+                       dict_el['salary'])
+
+    df2 = pd.DataFrame(dict_full['uii'],
+                       dict_full['total'])
+    writer = pd.ExcelWriter('output/agencies.xlsx', engine='xlsxwriter')
+    df1.to_excel(writer, sheet_name='Agencies')
+    df2.to_excel(writer, sheet_name='Individual Investments')
+    writer.save()
+    return dict_full
 
 
 def main():
-    browser_lib.create_webdriver('Chrome', options=options)
-    browser_lib.driver.get('https://itdashboard.gov/')
-    btn = browser_lib.driver.find_element_by_css_selector('a.btn.btn-default.btn-lg-2x')
-    btn.click()
-    time.sleep(1)
-    names = browser_lib.driver.find_elements_by_css_selector('span.h4.w200')
-    total_salary = browser_lib.driver.find_elements_by_css_selector('span.h1.w900')
-    name = [elem.text for elem in names if elem.text != '']
-    salary = [elem.text for elem in total_salary]
-    workbook = xlsxwriter.Workbook('output/agencies.xlsx')
-    worksheet = workbook.add_worksheet()
-    for i in range(0, len(name)-1):
-        worksheet.write(f'A{i+1}', name[i])
-        worksheet.write(f'B{i+1}', salary[i])
-    workbook.close()
+    open_browser_and_click_btn()
+    dict_agency = search_for_information()
+#   print(dict_agency)
+    url_agent = search_agent(dict_agency, 'Department of Agriculture')
+    dict_only_one = agent_full_information(url_agent)
+#   print(dict_only_one)
+    add_excel_agent(dict_only_one, dict_agency)
+    download_pdf(dict_only_one)
 
 
-def random_company():
-    main()
-    href = browser_lib.driver.find_elements_by_css_selector('a.btn.btn-default.btn-sm')
-    href_list = []
-    for h in href:
-        new_href = h.get_attribute('href')
-        href_list.append(new_href)
-    
-    browser_lib.driver.get(href_list[2])
-    time.sleep(10)
-    btn_select = browser_lib.driver.find_element_by_css_selector('select.form-control.c-select[aria-controls="investments-table-object"]')
-    btn_select.click()
-    btn_all = browser_lib.driver.find_element_by_css_selector('select.form-control.c-select[aria-controls] option[value="-1"]')
-    btn_all.click()
-    time.sleep(10)
-    dict_all = {
-        'uii': [el.text for el in browser_lib.driver.find_elements_by_css_selector('td.left.sorting_2')],
-        'uii_href': [el.get_attribute('href') for el in browser_lib.driver.find_elements_by_css_selector('td.left.sorting_2 a')],
-        'total': [el_1.text for el_1 in browser_lib.driver.find_elements_by_css_selector('td.right')]
-    }
-    workbook = xlsxwriter.Workbook('output/uii.xlsx')
-    worksheet = workbook.add_worksheet()
-    for i in range(0, len(dict_all['uii']) - 1):
-        worksheet.write(f'A{i + 1}', dict_all['uii'][i])
-        worksheet.write(f'B{i + 1}', dict_all['total'][i])
-    workbook.close()
-    for href1 in dict_all['uii_href']:
-        browser_lib.driver.get(href1)
-        time.sleep(5)
-        btn1 = browser_lib.driver.find_element_by_css_selector('div.row.top-gutter.tuck-4 a')
+def download_pdf(dict_only):
+    for href1 in dict_only['uii_href']:
+        driver.get(href1)
+        wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'div.row.top-gutter.tuck-4 a')))
+        btn1 = driver.find_element_by_css_selector('div.row.top-gutter.tuck-4 a')
         btn1.click()
         time.sleep(10)
 
 
-# Call the main() function, checking that we are running as a stand-alone script:
 if __name__ == "__main__":
-    random_company()
-#   main()
+    main()
