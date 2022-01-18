0961d311894072ffb076ae348296f91a2302e41b
smagliy <smaglijkata2002@gmail.com>
2022-01-18 14:59:41 +0200

    Done

--- pdf.py
+++ pdf.py
@@ -1,22 +1,48 @@
 import glob
-from PyPDF2 import PdfFileReader
-import pandas as pd
+import re
+from RPA.PDF import PDF
+from RPA.Excel.Files import Files
 
 
-def pdf_vs_xlsx():
-    list_files_pdf = glob.glob1("output/", "*.pdf")
-    df = pd.read_excel(io="output/new.xlsx", sheet_name='Individual Investments')
-    df.set_index("UII", drop=True, inplace=True)
-    dictionary = df.to_dict(orient="index")
-    for file in list_files_pdf:
-        print(file)
-        pdf = PdfFileReader(f"output/{file}")
-        page = pdf.getPage(0)
-        # print(page.extractText())
-        if dictionary[file[:-4]]['Bureau'] and dictionary[file[:-4]]['Investment title'] in page.extractText():
-            print('Comparison result:', True)
-        else:
-            print('Comparison result:', False)
+class PDFFiles(object):
+    """Class PDFFiles does: compare information from excel file and pdf file"""
+    def __init__(self):
+        self.glob = glob
+        self.pdf = PDF()
+        self.file = Files()
+
+    # Search files in folder "output"
+    def search_files(self):
+        list_files_pdf = self.glob.glob1("output/", "*.pdf")
+        return list_files_pdf
+
+    # Looking for information from excel and return list with info all uii
+    def looking_for_info_in_excel(self):
+        workbook = self.file.open_workbook('output/new_1.xlsx')
+        worksheet = workbook.read_worksheet('Individual Investments', header=True, start=1)
+        list_excel = []
+        for file in self.search_files():
+            name_file = file.replace(".pdf", '')
+            for worksh in worksheet:
+                if worksh['uii'] == name_file:
+                    list_excel.append(worksh)
+        return list_excel
+
+    # Compare info from excel and pdf
+    def info_from_files_pdf(self):
+        for i in range(0, len(self.search_files())-1):
+            file_path = f'output/{self.search_files()[i]}'
+            text = self.pdf.get_text_from_pdf(file_path, pages=1)
+            pdf_investment = re.findall(r'Investment:(.+?)2\.', text[1])[0].strip()
+            pdf_uui = re.findall(r'\(UII\): (.*?)Se', text[1])[0]
+            dict_values = self.looking_for_info_in_excel()[i]
+            if dict_values['uii'] == pdf_uui and dict_values['investment title'] == pdf_investment:
+                print(True)
+            else:
+                print(False)
+                print(dict_values['uii'], dict_values['investment title'])
+                print(pdf_uui, pdf_investment)
 
 
-pdf_vs_xlsx()
+pdf = PDFFiles()
+pdf.info_from_files_pdf()
---
+++ test_rpa.py
@@ -0,0 +1,102 @@
+from RPA.Browser.Selenium import Selenium
+from RPA.Excel.Files import Files
+from pdf import PDFFiles
+import time
+
+
+class ItDashBoard(object):
+    """Class ITDashBoard does: open the browser, add elements
+    to file with 2-sheet and download pdf files in folder 'output'"""
+    def __init__(self, agent):
+        self.browser = Selenium()
+        self.files = Files()
+        self.agent = agent
+
+    # Open browser
+    def open_site(self, url):
+        self.browser.open_available_browser(url)
+
+    # Click the button 'dive in'
+    def click_button(self):
+        self.open_site('https://itdashboard.gov/')
+        btn = self.browser.find_element('css:a.btn.btn-default.btn-lg-2x')
+        btn.click()
+
+    # Looking for information from table in site`s department and add to new_1.xlsx file
+    def search_informations(self):
+        self.click_button()
+        self.browser.wait_until_element_is_visible('css:a.btn.btn-default.btn-sm')
+        dict_elements = {
+            'button': [el.get_attribute('href') for el in self.browser.find_elements('css:a.btn.btn-default.btn-sm')],
+            'name': [el.text for el in self.browser.find_elements('css:span.h4.w200')],
+            'salary': [el.text for el in self.browser.find_elements('css:span.h1.w900')]
+        }
+        workbook = self.files.create_workbook('output/new_1.xlsx')
+        workbook.create_worksheet('Agencies')
+        workbook.append_worksheet('Agencies', {'name': dict_elements['name'], 'salary': dict_elements['salary']})
+        workbook.save()
+        return dict_elements
+
+    # Search index in dictionary['button']
+    def search_agent(self, dict_info):
+        index = dict_info['name'].index(self.agent)
+        return dict_info['button'][index]
+
+    # Collect information about agent and add to new_1.xlsx file
+    def agent_full_information(self, url):
+        self.browser.open_available_browser(url)
+        self.browser.wait_until_element_is_visible(
+            'css:select.form-control.c-select[aria-controls="investments-table-object"]', timeout=10)
+        btn = self.browser.find_element('css:select.form-control.c-select[aria-controls="investments-table-object"]')
+        btn.click()
+        btn_all = self.browser.find_element(
+            'css:select.form-control.c-select[aria-controls] option[value="-1"]')
+        btn_all.click()
+        time.sleep(15)
+        list_all = self.browser.find_elements('css:table[id="investments-table-object"] tbody tr[role="row"]')
+        dict_all = {
+            'uii': [],
+            'bureau': [],
+            'investment title': [],
+            'total': [],
+            'type': [],
+            'cio': [],
+            'projects': []
+        }
+        for row in list_all:
+            cols = self.browser.find_elements('css:td', parent=row)
+            dict_all['uii'].append(cols[0].text)
+            dict_all['bureau'].append(cols[1].text)
+            dict_all['investment title'].append(cols[2].text)
+            dict_all['total'].append(cols[3].text)
+            dict_all['type'].append(cols[4].text)
+            dict_all['cio'].append(cols[5].text)
+            dict_all['projects'].append(cols[6].text)
+        workbook = self.files.open_workbook('output/new_1.xlsx')
+        workbook.create_worksheet('Individual Investments')
+        workbook.append_worksheet('Individual Investments', dict_all,
+                                  header=['uii', 'bureau', 'investment title', 'total', 'type', 'cio', 'projects'])
+        workbook.save()
+        list_href = [el.get_attribute('href') for el in self.browser.find_elements('css:td.left.sorting_2 a')]
+        return list_href
+
+    # download pdf files
+    def links_to_go(self, hrefs):
+        self.browser.set_download_directory('/home/kate/PycharmProjects/Challenge/output')
+        for link in hrefs:
+            self.open_site(link)
+            self.browser.wait_until_element_is_visible('css:div.row.top-gutter.tuck-4 a')
+            self.browser.find_element('css:div.row.top-gutter.tuck-4 a').click()
+            time.sleep(10)
+
+    # all functions
+    def main(self):
+        dict_values = self.search_informations()
+        list_href = self.agent_full_information(self.search_agent(dict_values))
+        self.links_to_go(list_href)
+
+
+if __name__ == "__main__":
+    main = ItDashBoard('Department of State')
+    main.main()
+    PDFFiles().info_from_files_pdf()
